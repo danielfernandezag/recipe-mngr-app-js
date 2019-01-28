a63f8215344950beffdc5c58ce90512fbb487e4e
Julio Daniel Fernandez Aguirre <danielfernandezaguirre@outlook.com>
2019-01-28 09:57:00 -0600

    added particles-js

--- public/scripts/main.bundle.js
+++ public/scripts/main.bundle.js
@@ -1,6 +1,9 @@
 'use strict';
 
 window.addEventListener('load', function () {
+	particlesJS.load('particles', './assets/particles.config.json', function () {
+		console.log('callback - particles.js config loaded');
+	});
 	var login = document.getElementById('login-container');
 	var main = document.getElementById('main-container');
 	main.style.display = 'none';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', () => {
+	particlesJS.load('particles', './assets/particles.config.json', function() {
+		console.log('callback - particles.js config loaded');
+	});
 	const login = document.getElementById('login-container');
 	const main = document.getElementById('main-container');
 	main.style.display = 'none';
