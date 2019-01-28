ef6755203e435c35d79a036bc980d82357e0cf63
Julio Daniel Fernandez Aguirre <danielfernandezaguirre@outlook.com>
2019-01-28 00:00:34 -0600

    login page and styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-class SimplePerson {
-	constructor(age = 23, name = 'bob') {
-		this.age = age;
-		this.name = name;
-	}
-	getInfo() {
-		return `Hello my name is ${this.name} and I am ${this.age} years old`;
-	}
-}
-
-const newPerson = new SimplePerson(23, 'daniel');
-const domPlaceholder = document.getElementById('placeholder');
-domPlaceholder.innerHTML = newPerson.getInfo();
+window.addEventListener('load', () => {
+	const login = document.getElementById('login-container');
+	const main = document.getElementById('main-container');
+	main.style.display = 'none';
+});
